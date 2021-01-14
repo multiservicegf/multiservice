9f88926887cc2b4c14a88a59448e9a043525346f
Giovanni <giovannimf1309@hotmail.com>
2021-01-14 15:19:28 -0500

    1era actualizacion pag

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    //Slider
+    $('.bxslider').bxSlider({
+      auto: true,
+      autoControls: true,
+      stopAutoOnClick: true,
+      pager: true,
+      slideWidth: 1200,
+      adaptiveHeight:true
+    });
+})
